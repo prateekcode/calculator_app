eb7f4b9a743e44efade73cde51652bc6907477e4
Prateek <codes.prateek@gmail.com>
2020-06-16 12:15:51 +0530

    Output added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,32 +18,59 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Text(
+                "Output",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 20.0)),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(hintText: "Enter Number 1"),
+                
               ),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(hintText: "Enter Number 2"),
+                
               ),
               Padding(padding: EdgeInsets.only(top: 20.0)),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  MaterialButton(child: Text("+"), color: Colors.red, onPressed: null),
+                  MaterialButton(
+                      child: Text("+"),
+                      color: Colors.red,
+                      textColor: Colors.green,
+                      onPressed: null),
                   Padding(padding: EdgeInsets.only(left: 20.0)),
-                  MaterialButton(child: Text("-"), color: Colors.red, onPressed: null),
+                  MaterialButton(
+                      child: Text("-"),
+                      color: Colors.red,
+                      textColor: Colors.green,
+                      onPressed: null),
                 ],
               ),
               Padding(padding: EdgeInsets.only(top: 20.0)),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  MaterialButton(child: Text("*"), color: Colors.red, onPressed: null),
+                  MaterialButton(
+                      child: Text("*"),
+                      color: Colors.red,
+                      textColor: Colors.green,
+                      onPressed: null),
                   Padding(padding: EdgeInsets.only(left: 20.0)),
-                  MaterialButton(child: Text("/"),color: Colors.red, onPressed: null),
+                  MaterialButton(
+                      child: Text("/"),
+                      color: Colors.red,
+                      textColor: Colors.green,
+                      onPressed: null),
                 ],
-              )
+              ),
             ],
           ),
         ),
