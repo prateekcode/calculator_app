fd2bf176c13c3043121ed2efc4f1d382382ab5e5
Prateek <codes.prateek@gmail.com>
2020-06-16 12:36:43 +0530

    Calculator Finished

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,6 +6,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var num1 =0, num2 = 0, sum =0;
+  final TextEditingController t1 = TextEditingController(text:"0");
+  final TextEditingController t2 = TextEditingController(text:"0");
+
+  void doAddition(){
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 + num2;
+    });
+  }
+
+  void doSub(){
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 - num2;
+    });
+  }
+
+  void doMultiply(){
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 * num2;
+    });
+  }
+
+  void doDivide(){
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 ~/ num2;
+    }); 
+  }
+
+  void doClear(){
+    setState(() {
+      t1.text = "0";
+      t2.text = "0";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,12 +74,12 @@ class _HomePageState extends State<HomePage> {
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(hintText: "Enter Number 1"),
-                
+                controller: t1,
               ),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(hintText: "Enter Number 2"),
-                
+                controller: t2,
               ),
               Padding(padding: EdgeInsets.only(top: 20.0)),
               Row(
